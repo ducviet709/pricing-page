49d0360fe5420d2fbcaab800fcb2331a2214e42f
viet duc <ducviet709@gmail.com>
2020-07-08 17:14:50 +0700

    up

--- script.js
+++ script.js
@@ -1,50 +1,44 @@
-let amountInput = document.getElementById("amount");
+let guessesRemaining = 5
+let history = []
 
-let convertButton = document.getElementById('convertButton');
+// let reset = document.getElementById("reset")
 
-convertButton.addEventListener("click",conversionRate);
 
-function formatCurrency(type, value) {
-    const formatter = new Intl.NumberFormat(type, {
-      currency: type,
-      style: "currency"
-    });
-    return formatter.format(value);
-  }
+function guessNumber() {
+    guessesRemaining = 5 - history.length;
+    if (guessesRemaining == 0) {
 
-  const currencyRatio = {
-    vnd: {
-      usd: 0.000043,
-      krw: 0.051,
-      eur: 0.000039,
-      vnd: 1
-    },
-    usd: {
-      usd: 1,
-      krw: 1193.27,
-      eur: 0.9,
-      vnd: 23235.5
-    },
-    krw: {
-      usd: 0.00084,
-      krw: 1,
-      eur: 0.00075,
-      vnd: 19.47
+        resetGame()
+        return;
+
     }
-  };
-  
-  function getConversionRate(from, to) {
-         return currencyRatio[from.toLowerCase()][to.toLowerCase()];
-  }
+    else {
+        let userNumber = document.getElementById("userGuess").value
+        if (userNumber > randomNumber) {
+            document.getElementById("prompt").innerHTML = `Higher`
 
-  function conversionRate() {
-    
-    let from = document.getElementById("fromCurrencyList").value
-    let to = document.getElementById("toCurrencyList").value
-    let input = amountInput.value;
-    let exchangeRatio = getConversionRate(from,to)
-    let convertedAmount = formatCurrency(to,input * exchangeRatio)
-    const res = document.getElementById("result").innerHTML = `${convertedAmount}`
+        } else if (userNumber < randomNumber) {
+            document.getElementById("prompt").innerHTML = `Lower`
+        }
+        else if (userNumber == randomNumber) {
+            document.getElementById("prompt").innerHTML = `Correct`
+        }
+        history.push(userNumber)
+        console.log(history)
+        document.getElementById("historyArea").innerHTML = `${ history } `
+        document.getElementById("remain").innerHTML = `${ guessesRemaining } `
     }
+}
+
+function resetGame() {
+    guessesRemaining = 5
+    history = []
+    document.getElementById("prompt").innerHTML = ''
+    document.getElementById("remain").innerHTML = ''
+    document.getElementById("historyArea").innerHTML = ''
+    document.getElementById("userGuess").value = ''
+}
+
+let randomNumber = Math.floor(Math.random() * 100) + 1
 
 
